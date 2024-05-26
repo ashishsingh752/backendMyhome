3c55bc499e01301c62cc1dfb101f4fe5d40e1485
Ashish <theashish752@gmail.com>
2024-05-26 19:58:40 +0530

    adjust cors location

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ import cookieParser from "cookie-parser";
 import cors from 'cors'
 dotenv.config();
 
-app.use(cors({
-  origin : 'http://localhost:5173',
-  credentials : true
-})); 
 
 
 const app = express();
 
+app.use(cors({
+  origin : 'http://localhost:5173',
+  credentials : true
+})); 
 app.use(express.json());
 app.use(cookieParser());
 
