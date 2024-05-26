6b48365c83412e45b65b9a460a57d1041546aa97
Ashish <theashish752@gmail.com>
2024-05-26 19:36:18 +0530

    adjust folder structure

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/user", userRouter);
-app.use("/api/auth", authRouter);
-app.use("/api/listing", listingRouter);
+app.use("/user", userRouter);
+app.use("/auth", authRouter);
+app.use("/listing", listingRouter);
 
 // Error handling middleware
 app.use((err, req, res, next) => {
