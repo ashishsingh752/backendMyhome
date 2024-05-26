2c7c74646fc08c73df6a89c888b51fdccc4697ec
Ashish <theashish752@gmail.com>
2024-05-26 07:54:14 +0530

    connect with new database

--- api/index.js
+++ api/index.js
@@ -30,16 +30,13 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 3000;
 
-mongoose
-  .connect(process.env.MONGO, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+app.listen(PORT, ()=>{
+  console.log(`Server is connected to ${PORT} `);
+})
+
+mongoose.connect("process.env.MONGO")
   .then(() => {
     console.log("Connected to MongoDB!");
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}!`);
-    });
   })
   .catch((err) => {
     console.error("Error connecting to MongoDB:", err);
